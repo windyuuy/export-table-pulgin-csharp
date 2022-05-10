297dceccf97ba86b22695b9fad69eee7e094ad67
windyuuy <kugoo11v12@126.com>
2022-05-10 11:21:18 +0800

    U: 更新命令

--- test/test.bat
+++ test/test.bat
@@ -1,1 +1,1 @@
-call quicktable export src --one csharp:cs dist
+node E:\DATA\Projects\netease\quicktable\export-table\dist\launch.js export src dist --tags csharp:cs --libs E:\DATA\Projects\netease\quicktable %*
